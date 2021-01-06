e9b4b3207f2bd24352d655058a2530a97724a7ac
vinodhkumarkonda <vinodhkumar.prs@gmail.com>
2021-01-06 22:35:49 +0530

    Added weather component

--- src/app/weather/weather.module.ts
+++ src/app/weather/weather.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { WeatherRoutingModule } from './weather-routing.module';
+import { WeatherDetailsComponent } from './components/weather-details/weather-details.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [WeatherDetailsComponent],
   imports: [
     CommonModule,
     WeatherRoutingModule
