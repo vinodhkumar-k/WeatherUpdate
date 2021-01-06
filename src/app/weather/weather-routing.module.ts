aae3cb90dad2f4ffb7cfb5d0509c0e6c393a7d90
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2021-01-06 22:43:35 +0530

    Update weather-routing.module.ts

--- src/app/weather/weather-routing.module.ts
+++ src/app/weather/weather-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WeatherDetailsComponent } from './components/weather-details/weather-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: WeatherDetailsComponent
+  },
+  {
+    path: '**',
+    component: WeatherDetailsComponent,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
