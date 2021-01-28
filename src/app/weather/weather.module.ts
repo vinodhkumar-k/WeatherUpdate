894bb951e4d15f9627b7d5dacfb90b7bfc8e1e2f
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2021-01-28 20:39:13 +0530

    Update weather.module.ts

--- src/app/weather/weather.module.ts
+++ src/app/weather/weather.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 import { WeatherRoutingModule } from './weather-routing.module';
 import { WeatherDetailsComponent } from './components/weather-details/weather-details.component';
@@ -9,6 +15,13 @@ import { WeatherDetailsComponent } from './components/weather-details/weather-de
   declarations: [WeatherDetailsComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
     WeatherRoutingModule
   ]
 })
