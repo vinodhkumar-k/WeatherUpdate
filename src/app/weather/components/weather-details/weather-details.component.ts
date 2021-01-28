52ae415d98a7c83c931ab086f9a29d6a384737f1
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2021-01-28 20:43:42 +0530

    Update weather-details.component.ts

--- src/app/weather/components/weather-details/weather-details.component.ts
+++ src/app/weather/components/weather-details/weather-details.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-weather-details',
   templateUrl: './weather-details.component.html',
-  styleUrls: ['./weather-details.component.scss']
+  styleUrls: ['./weather-details.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class WeatherDetailsComponent implements OnInit {
+
+  name = new FormControl('');
 
   constructor() { }
 
