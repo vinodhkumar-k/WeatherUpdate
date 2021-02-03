f8d9ba81e9eaee7069a143a0a967327028454c2b
vinodhkumarkonda <vinodhkumar.prs@gmail.com>
2021-02-03 21:48:20 +0530

    Added weather data model

---
+++ src/app/weather/models/weather-data.model.ts
@@ -0,0 +1,10 @@
+export interface WeatherData {
+  name: string;
+  country: string;
+  date: Date;
+  temperature: number;
+  minTemperature: number;
+  maxTemperature: number;
+  weatherDiscription: string;
+  weatherIcon: string;
+}
