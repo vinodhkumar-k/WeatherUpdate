9595feb729a7885fb62f0600bd928ef071faf7ca
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2021-02-03 21:45:33 +0530

    Update weather-details.component.ts

--- src/app/weather/components/weather-details/weather-details.component.ts
+++ src/app/weather/components/weather-details/weather-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 import { WeatherDetailsService } from '../../services/weather-details.service';
+import { WeatherData } from '../../models/weather-data.model';
 
 
 @Component({
@@ -13,15 +14,29 @@ import { WeatherDetailsService } from '../../services/weather-details.service';
 export class WeatherDetailsComponent implements OnInit {
 
   name = new FormControl('');
+  weatherData: WeatherData[] = [];
+  minimizedCities = [];
+  // maximizedCities = [];
 
   constructor(private weatherDetailsService: WeatherDetailsService) { }
 
   getCityWeather() {
-    // console.log(this.name.value);
-    this.weatherDetailsService.getCity(this.name.value).subscribe(res => {
+    this.weatherDetailsService.getWeatherByCity(this.name.value).subscribe(res => {
       console.log(res);
-    })
+      this.weatherData.push({
+        name: res.name,
+        country: res.sys.country,
+        date: new Date(res.dt * 1000),
+        temperature: res.main.temp,
+        minTemperature: res.main.temp_min,
+        maxTemperature: res.main.temp_max,
+        weatherDiscription: res.weather.description,
+        weatherIcon: res.weather.icon
+      });
+    });
   }
+
+  onExpandCity(city, index) {}
 
   ngOnInit(): void {
   }
