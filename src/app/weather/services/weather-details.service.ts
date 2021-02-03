ebb3e1d165a0661e8eb0380745eaa823cb4080ff
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2021-02-03 21:47:28 +0530

    Update weather-details.service.ts

--- src/app/weather/services/weather-details.service.ts
+++ src/app/weather/services/weather-details.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable()
 
 export class WeatherDetailsService {
 
+  API_KEY = '495bc3c3676daf23d8e3b7c37bf380c5';
+  // weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=495bc3c3676daf23d8e3b7c37bf380c5';
+  weatherUrl = 'http://api.openweathermap.org/data/2.5/weather';
+
   constructor(private http: HttpClient) { }
 
-  getCity(cityName: string): Observable<any> {
-    return this.http.get('../../../assets/data/citylist.json');
-    // .subscribe((res: any) =>
-    // res.filter(city => city.name.toLowerCase() === cityName.toLowerCase()));
+  getWeatherByCity(cityName: string): Observable<any> {
+    const params = new HttpParams().set('q', cityName).set('APPID', this.API_KEY);
+    return this.http.get(this.weatherUrl, {params});
   }
 }
