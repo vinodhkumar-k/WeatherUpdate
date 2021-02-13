92bfd02d084ff493efd010e7e4a659e4a4815197
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2021-02-13 12:24:28 +0530

    Update shared.module.ts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
 import { AirQualityIconComponent } from './components/air-quality-icon/air-quality-icon.component';
+import { SnackbarService } from './services/snackbar.service';
 
 
 
 @NgModule({
   declarations: [AirQualityIconComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatSnackBarModule
+  ],
+  providers: [SnackbarService]
 })
 export class SharedModule { }
