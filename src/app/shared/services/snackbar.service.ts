b91bf3cd32142d15a2e030a12e61e1908597d790
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2021-02-13 12:23:56 +0530

    Update snackbar.service.ts

--- src/app/shared/services/snackbar.service.ts
+++ src/app/shared/services/snackbar.service.ts
@@ -1,11 +1,31 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 
 export class SnackbarService {
 
-  constructor() { }
+  constructor(private snackBar: MatSnackBar) { }
+
+  success(message: string) {
+    this.showSnackbar(message, 'success-snackbar');
+  }
+
+  error(message: string) {
+    this.showSnackbar(message, 'error-snackbar');
+  }
+
+  warn(message: string) {
+    this.showSnackbar(message, 'warn-snackbar');
+  }
+
+  showSnackbar(message, cssClass) {
+    this.snackBar.open(message, '', {
+      duration: 5000,
+      verticalPosition: 'top',
+      horizontalPosition: 'right',
+      panelClass: [ cssClass ]
+    });
+  }
 
 }
